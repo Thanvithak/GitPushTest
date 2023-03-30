4223508473b3d8695a526c3bd77b78e27e7b530b
LOCAL SERVICE <LOCAL SERVICE@QDH87TQ3>
2023-03-30 20:31:41 +0530

    NO COMMENT

---
+++ Test/connector/folder2/job_std_mstr_tgt_billingdiscounts_historical_tHiveRow_1_pre_hub_hive_fip_cem_uscc_com.sql
@@ -0,0 +1,39 @@
+
+insert into pre_hub.bill_disc_int  
+select
+bd_id
+,applied_date
+,sscr_id
+,cust_id
+,type_cd
+,promo_cd
+,sched_typ_cd
+,amount
+,current_timestamp as sys_insr_dt
+,'context.job_id' as insr_run_id
+,'context.import_date' as partition_key from
+
+(
+select
+bd_id
+,applied_date
+,sscr_id
+,cust_id
+,type_cd
+,promo_cd
+,sched_typ_cd
+,amount
+from pre_stage.bill_disc_int_bd_promo_stg_hist
+
+union all
+
+select
+bd_id
+,applied_date
+,sscr_id
+,cust_id
+,type_cd
+,promo_cd
+,sched_typ_cd
+,amount
+from pre_stage.bill_disc_int_bd_trail_stg_hist ) a
